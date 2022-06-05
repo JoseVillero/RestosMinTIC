2e1bac74bd53084fdc2bd93f4d460aefc1a84397
JoseVilleroSena <106927935+JoseVilleroSena@users.noreply.github.com>
2022-06-05 15:34:05 -0500

    Update reto_1_nutricion.py

--- retos_semana_1/Nutricion/reto_1_nutricion.py
+++ retos_semana_1/Nutricion/reto_1_nutricion.py
@@ -1,4 +1,5 @@
 # Reto 1 Nutricion
+# Realizado Por: Jose Villero ♠
 
 def solucion(edad, peso):
     edad = int(input('Indicar la edad del paciente: '))
@@ -8,7 +9,7 @@ def solucion(edad, peso):
     APORTE_P = 30.5          # Aporte proteinas
     APORTE_F_V = -24.4       # Aporte frutas y verduras
 
-    if 5 <= edad <= 10:
+    if 5 <= edad <= 10:     # Verificacion de edades entre 5 a 10 años
         if peso < 16:
             desnutricion = (APORTE_CH * 2) + (APORTE_P * 1) + (APORTE_F_V * 2)
             estado = 'A'
@@ -27,7 +28,8 @@ def solucion(edad, peso):
             dias = round(((28 - peso) / (pesoSaludable / 1000)) + 0.5)
             print(
                 f'El estado nutricional del paciente es {estado} y se requieren {dias} dias de dieta para que alcance el peso maximo')
-    elif 10 < edad <= 13:
+            
+    elif 10 < edad <= 13:           # Verificacion de edades entre 10 a 13 años
         if peso < 30:
             desnutricion = (APORTE_CH * 2) + (APORTE_P * 1) + (APORTE_F_V * 2)
             estado = 'A'
@@ -46,7 +48,8 @@ def solucion(edad, peso):
             dias = round(((50 - peso) / (pesoSaludable / 1000)) + 0.5)
             print(
                 f'El estado nutricional del paciente es {estado} y se requieren {dias} dias de dieta para que alcance el peso maximo')
-    elif 13 < edad <= 17:
+            
+    elif 13 < edad <= 17:           # Verificacion de edades entre 13 a 17 años
         if peso < 51:
             desnutricion = (APORTE_CH * 2) + (APORTE_P * 1) + (APORTE_F_V * 2)
             estado = 'A'
@@ -65,7 +68,8 @@ def solucion(edad, peso):
             dias = round(((63 - peso) / (pesoSaludable / 1000)) + 0.5)
             print(
                 f'El estado nutricional del paciente es {estado} y se requieren {dias} dias de dieta para que alcance el peso maximo')
-    else:
+            
+    else:          # Si la edad es mayor de 17 y menor de 15 muestra mensaje de error      
         if edad > 17:
             print('Error: La edad del paciente es mayor a 17 años')
         elif edad < 5:
