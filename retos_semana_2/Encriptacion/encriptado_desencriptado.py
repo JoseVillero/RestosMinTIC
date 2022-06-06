46917e21c2316961fd86ad54490d161d8b5c938c
JoseVilleroSena <106927935+JoseVilleroSena@users.noreply.github.com>
2022-06-06 11:59:20 -0500

    Add files via upload

---
+++ retos_semana_2/Encriptacion/encriptado_desencriptado.py
@@ -0,0 +1,16 @@
+mensaje = 'The Garden of Earthly Delights'    # Mensaje cualquiera a encriptar
+alfabeto = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'       # Alfabeto base para encriptar
+
+clave = set(mensaje)                           # convertimos el mensaje a conjunto
+clave = dict(zip(clave, alfabeto))             # unimos el conjunto con el alfabeto generando un diccionario
+encriptado = ''
+for i in mensaje:
+ encriptado += clave.get(i)                    # se obtienen los valores de las claves generando encriptacion
+clave = {v: k for k, v in clave.items()}       # Se invierten las claves por valores y viceversa
+desencriptado = ''
+for i in encriptado:
+ desencriptado += clave.get(i)                 # se obtienen los valores de las claves desencriptando el mensaje
+
+
+
+
